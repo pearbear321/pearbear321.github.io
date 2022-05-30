1c9b4058feb00f195341ca7486321a52489a3904
pearbear321 <105035650+pearbear321@users.noreply.github.com>
2022-05-30 18:24:06 -0500

    Update sheet.js

--- sheet.js
+++ sheet.js
@@ -1,14 +1,15 @@
-﻿const sheetID = '1L-RBWIHC6v3wWP64Lzu4ROcWTBc7BPNoiIgfsiLOeN8';
-const base = `https://docs.google.com/spreadsheets/d/${sheetID}gviz/tq?`;
+const sheetID = '1L-RBWIHC6v3wWP64Lzu4ROcWTBc7BPNoiIgfsiLOeN8';
+const base = `https://docs.google.com/spreadsheets/d/${sheetID}/gviz/tq?`;
 const sheetName = 'Index';
+const key = 'AIzaSyA9RqGBzlNr0zoo-LzULHm6IFFr3cttd10'
 const query = encodeURIComponent('Select *');
-const url = `${base}&sheet=${sheetName}&tq=${query}`
+const url = `${base}&sheet=${sheetName}&key=${key}&tq=${query}`
 const data = [];
 
 document.addEventListener('DOMContentLoaded', init);
 
 function init(){
-	console.log('ready');
+	console.log(url);
 	fetch(url)
 	.then(res => res.text())
 	.then(rep => {
