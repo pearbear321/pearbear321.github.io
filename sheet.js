9502d6618408fb101fefad7086fcf2fdf893c03f
pearbear321 <105035650+pearbear321@users.noreply.github.com>
2022-05-30 18:07:01 -0500

    Add files via upload

---
+++ sheet.js
@@ -0,0 +1,17 @@
+﻿const sheetID = '1L-RBWIHC6v3wWP64Lzu4ROcWTBc7BPNoiIgfsiLOeN8';
+const base = `https://docs.google.com/spreadsheets/d/${sheetID}gviz/tq?`;
+const sheetName = 'Index';
+const query = encodeURIComponent('Select *');
+const url = `${base}&sheet=${sheetName}&tq=${query}`
+const data = [];
+
+document.addEventListener('DOMContentLoaded', init);
+
+function init(){
+	console.log('ready');
+	fetch(url)
+	.then(res => res.text())
+	.then(rep => {
+		console.log(rep)
+	});
+}	
